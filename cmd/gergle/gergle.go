8b722fd4aa57fd1fd930595f8ce5298b71f052ad
Paul Scott <paul.scotto@gmail.com>
2016-02-07 09:50:48 +0000

    1 hour in

--- cmd/gergle/gergle.go
+++ cmd/gergle/gergle.go
@@ -1,25 +1,33 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 	"os"
+	"strings"
 	"sync"
 )
 
 type Page struct {
-	URL   string
-	HTML  bool
-	Depth int
-	Links []Link
+	URL       string
+	Processed bool
+	Depth     int
+	Links     []Link
+	Error     *error
 }
 
 type Link struct {
 	URL      string
 	External bool
+	Depth    int
 }
 
 type Task struct {
-	URL string
+	URL   string
+	Depth int
 }
 
 func main() {
@@ -48,13 +56,17 @@ func crawl(url string, out chan<- Page) {
 	go func() {
 		for task := range pending {
 			page := process(task)
-			for _, link := range page.Links {
-				if !link.External {
-					pending <- LinkTask(link)
-					tasks.Add(1)
+			out <- page
+
+			if page.Error == nil {
+				for _, link := range page.Links {
+					if !link.External {
+						// TODO: Don't requeue duplicates.
+						pending <- LinkTask(link)
+						tasks.Add(1)
+					}
 				}
 			}
-			out <- page
 			tasks.Done()
 		}
 	}()
@@ -64,13 +76,51 @@ func crawl(url string, out chan<- Page) {
 }
 
 func URLTask(url string) Task {
-	return Task{URL: url}
+	return Task{URL: url, Depth: 0}
 }
 
 func LinkTask(link Link) Task {
-	return URLTask(link.URL)
+	return Task{URL: link.URL, Depth: link.Depth}
+}
+
+func ErrorPage(url string, depth int, err error) Page {
+	return Page{url, false, depth, []Link{}, &err}
 }
 
 func process(task Task) Page {
-	return Page{task.URL, true, 0, []Link{}}
+	resp, err := http.Get(task.URL)
+	if err != nil {
+		return ErrorPage(task.URL, task.Depth, err)
+	}
+
+	defer resp.Body.Close()
+	return parsePage(task.URL, task.Depth, resp)
+}
+
+func parsePage(url string, depth int, resp *http.Response) Page {
+	if resp.StatusCode != 200 {
+		return ErrorPage(url, depth, errors.New("Non-200 response"))
+	}
+
+	mime := resp.Header.Get("Content-Type")
+	if !strings.Contains(strings.ToLower(mime), "html") {
+		return ErrorPage(url, depth, errors.New("Doesn't look like HTML"))
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return ErrorPage(url, depth, err)
+	}
+
+	base := parseBase(resp, body)
+	return Page{url, true, depth, parseLinks(base, body), nil}
+}
+
+func parseBase(resp *http.Response, body []byte) *url.URL {
+	return resp.Request.URL // TODO: Look for <base /> tags.
+}
+
+func parseLinks(base *url.URL, body []byte) []Link {
+	fmt.Printf("%s\n", body)
+	return []Link{}
 }
